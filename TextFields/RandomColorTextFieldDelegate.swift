6add4ddd1531f3d9ac156e441189122fef77642a
nicholszheng <nicolezshs@gmail.com>
2020-10-21 11:49:42 -0400

    Initial Commit

---
+++ TextFields/RandomColorTextFieldDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  RandomColorTextFieldDelegate.swift
+//  TextFields
+//
+//  Created by Nicole Zheng on 10/21/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RandomColorTextFieldDelegate: NSOBject, UITextFieldDelegate {
+    
+// func randomColor() -> UIColor {
+// @TODO: return a random color
+// }
+
+    private func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: (String) -> Bool, {
+    // @TODO: Set the color of your text here.
+        return true
+    }
+    
+}
